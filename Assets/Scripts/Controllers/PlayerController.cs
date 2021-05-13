e629cc6ff5f531e9d3a90bd3c9dba64ba73a1a69
csanchezarisa <61799628+csanchezarisa@users.noreply.github.com>
2021-05-13 14:03:16 +0200

    Movimiento de la pelota mejorado
    La pelota no empieza a volar

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -34,17 +34,38 @@ public class PlayerController : MonoBehaviour
         }
     }
 
-    void FixedUpdate()
+    void Update()
     {
-        float hMovment = Input.GetAxis("Horizontal");
-        float vMovement = Input.GetAxis("Vertical");
+        //float hMovement = Input.GetAxis("Horizontal");
+        //float vMovement = Input.GetAxis("Vertical");
 
-        Vector3 movement = new Vector3(hMovment, 0.0f, vMovement);
-        movement = camera.transform.rotation * movement;
-        rb.AddForce(movement * speed);
+        //Vector3 movement = new Vector3(camera.transform.forward.x * hMovement, 0, camera.transform.forward.z * vMovement);
+        //rb.AddForce(movement * speed);
+
+        //if (Input.GetButtonDown("Jump"))
+        //{
+        //    rb.AddForce(Vector3.up * speed, ForceMode.Impulse);
+        //}
+
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) + Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.1f)
+        {
+            Vector3 forward = camera.transform.TransformDirection(Vector3.forward);
+            forward.y = 0;
+            forward = forward.normalized;
+            Vector3 right = new Vector3(forward.z, 0, -forward.x);
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
+            Vector3 moveDirection = (h * right + v * forward);
+            moveDirection = Quaternion.Inverse(this.transform.rotation) * moveDirection;
+            moveDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
+            moveDirection = this.transform.rotation * moveDirection;
+            moveDirection.y = 0;
+
+            rb.AddForce(moveDirection * speed);
+        }
     }
 
-    private void OnTriggerEnter(Collider other)
+        private void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("GoldCoins"))
         {
