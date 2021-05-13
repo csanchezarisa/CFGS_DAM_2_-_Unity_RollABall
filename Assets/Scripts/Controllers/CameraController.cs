722cdbab37808cabee15b0ed9617fed7053cd3e8
csanchezarisa <61799628+csanchezarisa@users.noreply.github.com>
2021-05-13 12:52:38 +0200

    Camera movement X/Y

--- Assets/Scripts/Controllers/CameraController.cs
+++ Assets/Scripts/Controllers/CameraController.cs
@@ -5,20 +5,27 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     public GameObject player;
-    public float turnSpeed;
+    public float turnSpeed = 4;
 
-    private Vector3 offset;
+    public float height = 5f;
+    public float distance = 10f;
+
+    private Vector3 offsetX;
+    private Vector3 offsetY;
 
     // Start is called before the first frame update
     void Start()
     {
-        offset = new Vector3(player.transform.position.x, player.transform.position.y + 5.0f, player.transform.position.z + 7.0f);
+        offsetX = new Vector3(player.transform.position.x, player.transform.position.y + height, player.transform.position.z + distance);
+        offsetY = new Vector3(player.transform.position.x, player.transform.position.y + height, player.transform.position.z + distance);
     }
 
     void LateUpdate()
     {
-        offset = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offset;
-        transform.position = player.transform.position + offset;
+        offsetX = Quaternion.AngleAxis(Input.GetAxis("Mouse X") * turnSpeed, Vector3.up) * offsetX;
+        offsetY = Quaternion.AngleAxis(Input.GetAxis("Mouse Y") * turnSpeed, Vector3.right) * offsetY;
+
+        transform.position = player.transform.position + offsetX + offsetY;
         transform.LookAt(player.transform);
     }
 }
