0f0cbf143b4f0adf2df9042d19e992c2365a3b06
ulkiobal <ulkiobal@users.noreply.github.com>
2021-05-23 10:34:05 +0200

    Movimiento del player mejorado
    El movimiento del player se dirige según la rotación del cubo shooter y no de la cámara.
    El cubo shooter se rota según la cámara

--- Assets/Scripts/Controllers/PlayerController.cs
+++ Assets/Scripts/Controllers/PlayerController.cs
@@ -8,7 +8,7 @@ public class PlayerController : MonoBehaviour
 
     public float speed = 5;
     public float jumpForce = 10;
-    public GameObject camera;
+    public Transform playerShooter;
     public Transform desertRespawn;
     public Transform riverRespawn;
 
@@ -24,25 +24,17 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         // Movimiento horizontal
-        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) + Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.1f)
+        if (Mathf.Abs(Input.GetAxisRaw("Vertical")) + Mathf.Abs(Input.GetAxisRaw("Horizontal")) > 0.01f)
         {
-            Vector3 forward = camera.transform.TransformDirection(Vector3.forward);
-            forward.y = 0;
-            forward = forward.normalized;
-            Vector3 right = new Vector3(forward.z, 0, -forward.x);
             float h = Input.GetAxis("Horizontal");
             float v = Input.GetAxis("Vertical");
-            Vector3 moveDirection = (h * right + v * forward);
-            moveDirection = Quaternion.Inverse(this.transform.rotation) * moveDirection;
-            moveDirection = new Vector3(moveDirection.x, 0, moveDirection.z);
-            moveDirection = this.transform.rotation * moveDirection;
-            moveDirection.y = 0;
+            Vector3 moveDirection = (h * playerShooter.right + v * -playerShooter.forward);
 
             rb.AddForce(moveDirection * speed, ForceMode.Force);
         }
 
         // Salto
-        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.01f)
+        if (Input.GetButtonDown("Jump") && Mathf.Abs(rb.velocity.y) < 0.1f)
             Jump();
     }
 
