4d0fdffa57922b3dde4d7aa8d5f2f16a67d2fbc7
ulkiobal <ulkiobal@users.noreply.github.com>
2021-05-25 16:43:47 +0200

    Escenario pasillo con enemigos
    Los enemigos disparan en la dirección donde está el jugador

--- Assets/Scripts/Controllers/EnemyController.cs
+++ Assets/Scripts/Controllers/EnemyController.cs
@@ -7,18 +7,46 @@ public class EnemyController : MonoBehaviour
 
     public Transform player;
     public GameObject bulletPrefab;
-    public float bulletSpeed;
+    public float bulletSpeed = 100;
 
     private AudioSource audioShoot;
+    private bool waitToShoot = false;
 
     void Start()
     {
         audioShoot = GetComponent<AudioSource>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
-        
+        transform.LookAt(player);
+
+        if (!waitToShoot)
+        {
+            float time = Random.Range(3, 10);
+            StartCoroutine(WaitToShoot(time));
+        }
+
+    }
+
+    private void Shoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, transform.position, transform.rotation) as GameObject;
+        Rigidbody bulletRb = bullet.GetComponent<Rigidbody>();
+
+        audioShoot.Play();
+
+        bulletRb.AddForce(transform.forward * bulletSpeed, ForceMode.Impulse);
+
+        Destroy(bullet, 3);
+        Destroy(bulletRb, 3);
+    }
+
+    IEnumerator WaitToShoot(float time)
+    {
+        this.waitToShoot = true;
+        yield return new WaitForSeconds(time);
+        Shoot();
+        this.waitToShoot = false;
     }
 }
